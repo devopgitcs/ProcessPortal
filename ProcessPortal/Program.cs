9ed221e3d9204ad5ad6d11737a6f9d27d66bed9c
Chandra.Lakkims-PTR <Chandra.Lakkims-PTR@kiewit.com>
2025-04-03 16:58:43 -0500

    added portal files

--- ProcessPortal/Program.cs
+++ ProcessPortal/Program.cs
@@ -1,8 +1,9 @@
-// ProcessPortal/Program.cs
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Define a simple GET endpoint
 app.MapGet("/", () => "Hello from .NET 8 Minimal API!");
 
 app.Run();
